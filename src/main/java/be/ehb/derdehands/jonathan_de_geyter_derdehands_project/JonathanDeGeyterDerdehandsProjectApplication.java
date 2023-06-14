22ed0b7024b10d0b337aa5faa376b4f2a14ef678
AmicableAlien aka Jono_DG <jonathan.de.geyter@student.ehb.be>
2023-06-14 23:42:56 +0100

    Initialising project 'derdehands'

---
+++ src/main/java/be/ehb/derdehands/jonathan_de_geyter_derdehands_project/JonathanDeGeyterDerdehandsProjectApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.derdehands.jonathan_de_geyter_derdehands_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonathanDeGeyterDerdehandsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JonathanDeGeyterDerdehandsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/derdehands/jonathan_de_geyter_derdehands_project/JonathanDeGeyterDerdehandsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.derdehands.jonathan_de_geyter_derdehands_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonathanDeGeyterDerdehandsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
